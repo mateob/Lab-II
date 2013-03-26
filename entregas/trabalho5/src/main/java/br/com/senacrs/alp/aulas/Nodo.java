2ad5459717694a1ef4a47d7b977dd0370c7d6ce3
mateob <xd2006@gmail.com>
2013-03-26 19:32:59 -0300

    Trabalho 6

---
+++ entregas/trabalho5/src/main/java/br/com/senacrs/alp/aulas/Nodo.java
@@ -0,0 +1,9 @@
+package br.com.senacrs.alp.aulas;
+
+public class Nodo<T> {
+	
+	public T conteudo;
+	
+	public Nodo<T> proximo;
+
+}
---
+++ entregas/trabalho6/src/main/java/br/com/senacrs/alp/aulas/MinhaPilha.java
@@ -0,0 +1,43 @@
+package br.com.senacrs.alp.aulas;
+
+public class MinhaPilha<T> implements Pilha<T> {
+
+	private Nodo<T> inicio = new Nodo<T>();
+	
+	@Override
+	public void insercao(T valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T obter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T remocao() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean vazia() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int tamanho() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void esvaziar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
