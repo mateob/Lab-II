345ba136b34ba97c540536f82085c6cfa5d9e452
mateob <xd2006@gmail.com>
2013-04-11 20:31:57 -0300

    Trabalho 7

--- entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparadorInt.java
+++ entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparadorInt.java
@@ -3,10 +3,10 @@ package br.com.senacrs.alp.aulas;
 import java.util.Comparator;
 
 public class MeuComparadorInt<Integer> implements Comparator<Integer>{
-
+	
 	@Override
 	public int compare(Integer o1, Integer o2) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
