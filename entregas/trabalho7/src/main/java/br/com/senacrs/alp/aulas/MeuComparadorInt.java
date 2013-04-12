9a809b176151506d1665705c71d2e21bb7047de0
mateob <xd2006@gmail.com>
2013-04-11 21:42:14 -0300

    Trabalho 7

--- entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparadorInt.java
+++ entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparadorInt.java
@@ -1,11 +1,17 @@
 package br.com.senacrs.alp.aulas;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 
-public class MeuComparadorInt<Integer> implements Comparator<Integer>{
+public class MeuComparadorInt implements Comparator<Integer>{
 	
 	@Override
 	public int compare(Integer o1, Integer o2) {
+		Integer[] intArray = {};
+		
+		
+		Arrays.sort(intArray);
 		
 		return 0;
 	}
--- entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparadorStr.java
+++ entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparadorStr.java
@@ -2,11 +2,11 @@ package br.com.senacrs.alp.aulas;
 
 import java.util.Comparator;
 
-public class MeuComparadorStr<String> implements Comparator<String> {
+public class MeuComparadorStr implements Comparator<String> {
 
 	@Override
 	public int compare(String o1, String o2) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
