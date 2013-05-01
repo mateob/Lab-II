3a0fdf8c715589721ff951f5a694bd702dff90bd
mateob <xd2006@gmail.com>
2013-04-30 21:18:27 -0300

    trabalho 9

--- entregas/trabalho9/src/main/java/br/com/senacrs/alp/aulas/MemissordeMenj.java
+++ entregas/trabalho9/src/main/java/br/com/senacrs/alp/aulas/MemissordeMenj.java
@@ -17,84 +17,22 @@ public class MemissordeMenj implements EmissorMensagens {
 		if(chave == null){
 			throw new IllegalArgumentException();
 		}
-		
-		/* chave <string> = <string> argumento */
-		BufferedReader leitor = null;
-		
-		leitor = obterBuffer(chave);
-		resultado = lerBuffer(leitor);
-		return resultado;
-	}
-
-	private String lerBuffer(BufferedReader leitor) {
-		String resultado = null;
-		String linha = null;
-		ArrayList<String> lista = null;
-		
-		lista = new ArrayList<String>();
-		
-		try{
-			linha = leitor.readLine();
-			while(linha !=null){
-				lista.add(linha);
-				linha = leitor.readLine();
+		if(ehBemFormatada(chave, argumentos)){
 			
-			}
-		}catch(IOException e){
-			throw new IllegalStateException(e);
 		}
-		return resultado;
-	}
-
-	
-	private BufferedReader obterBuffer(String chave) {
-		BufferedReader resultado = null;
-		File file = null;
-		FileReader reader = null;
-		
-		file = obterFile(chave);
-		try{
-			reader = new FileReader(file);
-			resultado = new BufferedReader(reader);
-		}catch(FileNotFoundException e){
-			throw new IllegalStateException(e);
-		}
-		
-		return resultado;
-	}
-
-	private File obterFile(String chave) {
-		File resultado = null;
-		
-		verificarNomeArquivo(chave);
-		resultado = new File(chave);
-		verificarFile(resultado);
-		
-		return resultado;
-	}
-
-	private void verificarFile(File resultado) {
+		resultado = String.format(chave, argumentos);
 		if(resultado == null){
 			throw new IllegalArgumentException();
 		}
-		if(!resultado.exists()){
-			throw new IllegalArgumentException();
-		}
-		if(!resultado.isFile()){
-			throw new IllegalArgumentException();
-		}
-		if(!resultado.canRead()){
-			throw new IllegalArgumentException();
-		}
+		/* chave <string> = <string> argumento */
 		
-		
+		return resultado;
 	}
 
-	private void verificarNomeArquivo(String chave) {
-		if(chave == null){
-			throw new IllegalArgumentException();
-		}
-		
-		chave = chave.trim();
+	private boolean ehBemFormatada(String chave, Object[] argumentos) {
+				
+		return false;
 	}
+
+	
 }
